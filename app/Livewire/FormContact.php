f3a4be473d1e774a3c5d863eaf2b99e99a0d014e
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-06 14:41:31 -0300

    007(OutraForma_Validacao)

--- app/Livewire/FormContact.php
+++ app/Livewire/FormContact.php
@@ -3,31 +3,29 @@
 namespace App\Livewire;
 
 use Illuminate\Support\Facades\Log;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class FormContact extends Component
 {
+    
+    #[Validate("required|min:3|max:50")]
+    public $name;
 
-    public $name, $email, $phone;
+    #[Validate("required|email|min:5|max:50")]
+    public $email;
+
+    #[Validate("required|min:5|max:20")]
+    public $phone;
 
     public function newContact() {
         //Validation:
-        $this->validate([
-            "name" => "required|min:3|max:50",
-            "email" => "required|email|min:5|max:50",
-            "phone" => "required|min:5|max:20",
-        ]);
+        $this->validate();
 
         //Temporary storage in log file:
         Log::info("Novo contato: " . $this->name . " - " . $this->email . " - " . $this->phone);
 
         //Clear form:
-        //Option 1:
-        // $this->name = "";
-        // $this->email = "";
-        // $this->phone = "";
-
-        //Option 2:
         $this->reset();
     }
 
