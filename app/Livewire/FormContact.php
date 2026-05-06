b492907477ad9ed94b5efff2f564c4e67eeac2db
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-06 13:33:26 -0300

    005(ValidacaoFormulario_NovoContacto)

--- app/Livewire/FormContact.php
+++ app/Livewire/FormContact.php
@@ -2,10 +2,26 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class FormContact extends Component
 {
+
+    public $name, $email, $phone;
+
+    public function newContact() {
+        //Validation:
+        $this->validate([
+            "name" => "required|min:3|max:50",
+            "email" => "required|email|min:5|max:50",
+            "phone" => "required|min:5|max:20",
+        ]);
+
+        //Temporary storage in log file:
+        Log::info("Novo contato: " . $this->name . " - " . $this->email . " - " . $this->phone);
+    }
+
     public function render()
     {
         return view('livewire.form-contact');
