0be3ef4c06c89b151845b156f185fb0f800d0466
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-07 09:43:48 -0300

    011(Apresentar_Contacts)

--- resources/views/livewire/contacts.blade.php
+++ resources/views/livewire/contacts.blade.php
@@ -1,5 +1,13 @@
 <div class="card p-5">
-    <p>Contacts</p>
+    <p class="mb-3">Contacts</p>
     
-    <hr class="m-0 p-0">
+    @foreach ($contacts as $contact)
+        <div class="card p-3 mb-1">
+            <div class="row">
+                <div class="col">Name: {{ $contact->name }}</div>
+                <div class="col">Email: {{ $contact->email }}</div>
+                <div class="col">Phone: {{ $contact->phone }}</div>
+            </div>
+        </div>
+    @endforeach
 </div>
